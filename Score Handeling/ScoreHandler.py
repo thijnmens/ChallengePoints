5054acd898ef162e55b4aab57eaae92356c2c817
thijnmens <thijnsmulders04@gmail.com>
2021-01-15 13:46:23 +0100

    Thx Nyanbre#1578 for this amazing formula

--- Score Handeling/ScoreHandler.py
+++ Score Handeling/ScoreHandler.py
@@ -8,8 +8,13 @@ header = {
 def getScore(userID):
     weight = int(input('What is the weight of the map?:\n'))
     acc = float(input('What score did you get?:\n'))
-    cpm = (50/(0.5+(math.e**(-1*(0.08*(acc-65.4))))))/100
-    cp = round(weight*cpm, 2)
+    f = (-0.0000000089146*acc**5)+(0.0000036255812*acc**4)+(-0.0004061889074*acc**3)+(0.003306769423*acc**2)+(1.9970958028178*acc)
+    if (0 <= acc <= 135):
+        g = (2*acc)-(f)
+        h = g/100
+    else:
+        h = 0
+    cp = round(weight*h, 2)
     print(f'You got {cp}CP!')
 
 userID = 0
